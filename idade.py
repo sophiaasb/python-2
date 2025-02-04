2713b128cf30fbf98fa0d7112ee68e7e68ff4328
Sophia Angelina <161032086+sophiaasb@users.noreply.github.com>
2025-02-04 18:49:35 -0300

    Delete idade.py

--- idade.py
+++
@@ -1,8 +0,0 @@
-idade = int(input('Digite sua idade: '))
-
-if(idade>=18):
-    print('Você é adulto')
-elif(idade<18 and idade>=12):
-    print('Você é adolescente.')
-else:
-    print('Você é criança.')
