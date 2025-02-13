aae62fc92191b9992daaba09442c143ed2d12af3
Sophia Angelina <161032086+sophiaasb@users.noreply.github.com>
2025-02-13 15:04:25 -0300

    Add files via upload

---
+++ q3.py
@@ -0,0 +1,12 @@
+numeros = []
+
+for i in range(10):
+    num = int(input(f"Digite o {i+1}º número: "))
+    numeros.append(numeros)
+
+maior_numero = max(numeros)
+soma = sum(numeros)
+
+print(f"A lista dos números: {numeros}")
+print(f"O maior número é: {maior_numero}")
+print(f"A soma dos números é: {soma}")
