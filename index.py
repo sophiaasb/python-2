da3aa1289b829776a26adb81ee29496595af11ae
Sophia Angelina <161032086+sophiaasb@users.noreply.github.com>
2025-05-29 16:13:25 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,28 @@
+import openpyxl
+from docxtpl  import DocxTemplate 
+import datetime
+
+# acessar a planilha e consumir dados
+
+caminho = 'student_data.xlsx'
+pasta_trabalho = openpyxl.load_workbook(caminho)
+planilha = pasta_trabalho.active
+
+valores = list(planilha.values)
+
+print(valores)
+
+# gerando o certificado preenchido
+
+doc = DocxTemplate('certificate.docx')
+
+for x in valores[1:]:
+    doc.render({
+        'nome':x[0],
+        'curso':x[1],
+        'data':x[2],
+        'instrutor':x[3]
+    })
+    
+    nome = f'certificado de {x[0]}.docx'
+    doc.save(nome)
